22f57677958fec7aa04f21e49e701e8615fa349d
Florent Jaby <florent.jaby@gmail.com>
2017-07-06 16:54:23 +0200

    add default value and override

--- test/descriptor.test.js
+++ test/descriptor.test.js
@@ -52,6 +52,78 @@ describe('Descriptor.description(name, validator, value)', () => {
       })
     })
   })
+
+  describe('when the validator is .string()', () => {
+    const desc = Joi.string()
+    describe('the first line', () => {
+      it('contains the name and type with colours', () => {
+        return description(name, desc)
+          .pipe(sink())
+          .then(getLine(0))
+          .then((line) => {
+            expect(line).to.equal(`  ${name.bold.cyan} (${'string'.italic})`)
+          })
+      })
+    })
+  })
+
+  describe('when there is a value setup', () => {
+    const desc = Joi.string().default('World')
+    describe('different from the default', () => {
+      const value = "Hello"
+      describe('and no description', () => {
+        describe('the second line', () => {
+          it('contains the overloaded value', () => {
+            return description(name, desc, value)
+              .pipe(sink())
+              .then(getLine(1))
+              .then((line) => {
+                expect(line).to.equal(`    overwritten: ${value.bold.green}`)
+              })
+          })
+        })
+      })
+      describe('and a description', () => {
+        const desc = Joi.string().description('something')
+        describe('the third line', () => {
+          it('contains the overloaded value', () => {
+            return description(name, desc, value)
+              .pipe(sink())
+              .then(getLine(2))
+              .then((line) => {
+                expect(line).to.equal(`    overwritten: ${value.bold.green}`)
+              })
+          })
+        })
+      })
+    })
+
+    describe('equal to the default', () => {
+      const value = "World"
+      describe('the description', () => {
+        it('does not mention it', () => {
+          return description(name, desc, value)
+            .pipe(sink())
+            .then((text) => {
+              expect(text).not.to.contain(value)
+            })
+        })
+      })
+    })
+
+    describe('to undefined', () => {
+      let value
+      describe('the description', () => {
+        it('does not mention it', () => {
+          return description(name, desc, value)
+            .pipe(sink())
+            .then((text) => {
+              expect(text).not.to.contain(value)
+            })
+        })
+      })
+    })
+  })
 })
 
 function getLine (index) {
