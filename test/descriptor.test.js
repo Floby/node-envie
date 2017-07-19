ba9e5e5bc9250630339ccb41a83ab02624bc01f4
Florent Jaby <florent.jaby@gmail.com>
2017-07-19 16:12:58 +0200

    Add support for uri schemes and default and validation

--- test/descriptor.test.js
+++ test/descriptor.test.js
@@ -53,6 +53,20 @@ describe('Descriptor.description(name, validator, value)', () => {
     })
   })
 
+  describe('when there is a default value', () => {
+    const desc = Joi.number().default(8)
+    describe('the first line', () => {
+      it('contains the default value after the type', () => {
+        return description(name, desc)
+          .pipe(sink())
+          .then(getLine(0))
+          .then((line) => {
+            expect(line).to.equal(`  ${name.bold.cyan} (${'number'.italic}), default: ${'8'.gray}`)
+          })
+      })
+    })
+  })
+
   describe('when the validator is .string()', () => {
     const desc = Joi.string()
     describe('the first line', () => {
@@ -63,6 +77,48 @@ describe('Descriptor.description(name, validator, value)', () => {
           .then((line) => {
             expect(line).to.equal(`  ${name.bold.cyan} (${'string'.italic})`)
           })
+      })
+    })
+
+    describe('and .uri({scheme})', () => {
+      context('when scheme is undefined', () => {
+        const desc = Joi.string().uri()
+        describe('the first line', () => {
+          it('contains the name and type with colours', () => {
+            return description(name, desc)
+              .pipe(sink())
+              .then(getLine(0))
+              .then((line) => {
+                expect(line).to.equal(`  ${name.bold.cyan} (${'uri'.italic})`)
+              })
+          })
+        })
+      })
+      context('when there is one scheme', () => {
+        const desc = Joi.string().uri({ scheme: 'http' })
+        describe('the first line', () => {
+          it('contains the name, type and declinations with colours', () => {
+            return description(name, desc)
+              .pipe(sink())
+              .then(getLine(0))
+              .then((line) => {
+                expect(line).to.equal(`  ${name.bold.cyan} (${'uri <http>'.italic})`)
+              })
+          })
+        })
+      })
+      context('when there are many schemes', () => {
+        const desc = Joi.string().uri({ scheme: ['http', 'https'] }).uri({ scheme: 'ftp' })
+        describe('the first line', () => {
+          it('contains the name, type and declinations with colours', () => {
+            return description(name, desc)
+              .pipe(sink())
+              .then(getLine(0))
+              .then((line) => {
+                expect(line).to.equal(`  ${name.bold.cyan} (${'uri <http|https|ftp>'.italic})`)
+              })
+          })
+        })
       })
     })
   })
@@ -84,7 +140,7 @@ describe('Descriptor.description(name, validator, value)', () => {
         })
       })
       describe('and a description', () => {
-        const desc = Joi.string().description('something')
+        const desc = Joi.string().default('World').description('something')
         describe('the third line', () => {
           it('contains the overloaded value', () => {
             return description(name, desc, value)
@@ -105,7 +161,7 @@ describe('Descriptor.description(name, validator, value)', () => {
           return description(name, desc, value)
             .pipe(sink())
             .then((text) => {
-              expect(text).not.to.contain(value)
+              expect(text.split('\n')).to.have.length(2)
             })
         })
       })
@@ -119,6 +175,21 @@ describe('Descriptor.description(name, validator, value)', () => {
             .pipe(sink())
             .then((text) => {
               expect(text).not.to.contain(value)
+            })
+        })
+      })
+    })
+
+    describe('invalid', () => {
+      const desc = Joi.number()
+      let value = 'hello'
+      describe('the value line', () => {
+        it('explains the validation error', () => {
+          return description(name, desc, value)
+            .pipe(sink())
+            .then(getLine(1))
+            .then((line) => {
+              expect(line).to.equal(`    overwritten: ` + `invalid! "${name}" must be a number`.red)
             })
         })
       })
